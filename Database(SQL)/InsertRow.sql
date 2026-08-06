bbfc1e33bf4ccf51199525b83a3fc7bcce824295
deepchandra633 <kushwahdeepchandra640@gmail.com>
2026-08-06 10:13:44 +0530

    add

---
+++ Database(SQL)/InsertRow.sql
@@ -0,0 +1,50 @@
+-- CREATE DATABASE schooldb;
+-- USE schooldb
+
+-- CREATE TABLE student(
+-- id INT PRIMARY KEY,
+-- name VARCHAR(100) ,
+-- age INT ,
+-- grade VARCHAR(10),
+-- dob DATE 
+-- );
+
+-- SHOW TABLES;
+
+-- INSERT INTO student (id , name , age , grade , dob) 
+-- VALUES(1,"Deppchandra",21,"Mtech","2005-05-03");
+--  SELECT * FROM student
+
+-- INSERT INTO student (id , name , age , grade , dob) 
+-- VALUES(2,"rahul",22,"10th","2006-08-03");
+-- INSERT INTO student (id , name , age , grade , dob) 
+-- VALUES(3,"shyam",25,"12th","2008-05-03");
+-- INSERT INTO student (id , name , age , grade , dob) 
+-- VALUES(4,"Bhavna",22,"btech","2005-03-25");
+-- INSERT INTO student (id , name , age , grade , dob) 
+-- VALUES(5,"sachin",19,"12th","2007-05-03");
+-- INSERT INTO student (id , name , age , grade , dob) 
+-- VALUES(6,"dev",15,"5th",NULL);
+
+INSERT INTO student (id, name, age, grade) VALUES
+(15, 'Ayesha Khan', 16, '10th'),
+(25, 'Ravi Sharma', 17, '11th'),
+(35, 'Meena Joshi', 15, '9th'),
+(45, 'Arjun Verma', 18, '12th'),
+(55, 'Sara Ali', 16, '10th'),
+(65, 'Karan Mehta', 17, '11th'),
+(75, 'Tanya Roy', 15, '9th'),
+(85, 'Vikram Singh', 18, '12th'),
+(95, 'Anjali Desai', 16, '10th'),
+(105, 'Farhan Zaidi', 17, '11th');
+SELECT * FROM student
+
+
+
+
+
+
+
+
+
+
